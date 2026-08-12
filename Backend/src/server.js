c7f15be296298f76684d04ce46de385342b80a60
pallavijain-cell <pallavijain@igtechso.com>
2026-08-13 02:56:41 +0530

    deploy on vercel

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,7 +10,12 @@ const overtimeRoutes = require('./routes/overtimeRoutes');
 const userRoutes = require('./routes/userRoutes');
 const app = express();
 
-app.use(cors());
+app.use(
+ cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(morgan("dev"));
